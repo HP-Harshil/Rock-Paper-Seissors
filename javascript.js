64ac76b385d5938ffcbf7df414b6032fdf9bd20d
Harshil <parekhharshil99@gmail.com>
2022-11-29 10:35:25 +0530

    getComputerChoise function crated

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
 console.log("Hello")
+
+function getComputerChoise() {
+    let i
+    i = Math.floor(Math.random()*3)
+
+    let x
+    if (i === 0) {
+        x = "Rock"
+    } else if (i === 1) {
+        x = "Paper"
+    } else {
+        x = "Seissors"
+    }
+
+    return x
+}
+
+getComputerChoise()
+
