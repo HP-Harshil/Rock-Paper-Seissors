f460ca9e93d50e35f9ef5837b9bbd47df371079f
Harshil <parekhharshil99@gmail.com>
2022-11-29 11:57:43 +0530

    playRound function created

--- javascript.js
+++ javascript.js
@@ -6,15 +6,44 @@ function getComputerChoise() {
 
     let x
     if (i === 0) {
-        x = "Rock"
+        x = "rock"
     } else if (i === 1) {
-        x = "Paper"
+        x = "paper"
     } else {
-        x = "Seissors"
+        x = "seissors"
     }
 
     return x
 }
 
-getComputerChoise()
+function playRound(playerSelection, computerSelection){
 
+    playerSelection = playerSelection.toLowerCase()
+    
+    let winner
+
+    if (playerSelection == computerSelection) {
+        return "It's tie!"
+    } else if (playerSelection == "rock") {
+        computerSelection == "paper" ? winner = "Computer" : winner = "You"
+    } else if (playerSelection == "paper") {
+        computerSelection == "seissors" ? winner = "Computer" : winner = "You"
+    } else if (playerSelection == "seissors") {
+        computerSelection == "rock" ? winner = "Computer" : winner = "You"
+    }
+    
+    let string
+
+    if (winner == "Computer") {
+        string = computerSelection[0].toUpperCase() + computerSelection.substring(1) + " beats " + playerSelection[0].toUpperCase() + playerSelection.substring(1)
+    } else if (winner == "You") {
+        string = playerSelection[0].toUpperCase() + playerSelection.substring(1) + " beats " + computerSelection[0].toUpperCase() + computerSelection.substring(1)
+    }
+
+    return winner + " wins! " + string
+
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoise()
+console.log(playRound(playerSelection, computerSelection))
