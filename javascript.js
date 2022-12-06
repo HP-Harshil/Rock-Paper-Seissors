7b9d85762817c6835b63ecca9cd190fef7480606
Harshil <parekhharshil99@gmail.com>
2022-12-06 23:43:36 +0530

    ui made

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-let playerSelection = prompt()
-
 function getComputerChoise() {
     let i
     i = Math.floor(Math.random()*3)
@@ -16,44 +14,77 @@ function getComputerChoise() {
     return x
 }
 
-let computerSelection
+let computerSelection;
+// let playerSelection = 'Rock';
 
 function playRound(playerSelection, computerSelection){
 
-    playerSelection = playerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
     
-    let winner
+    let winner;
 
     if (playerSelection == computerSelection) {
-        return "It's tie!"
+        return "It's tie!";
     } else if (playerSelection == "rock") {
-        computerSelection == "paper" ? winner = "Computer" : winner = "You"
+        computerSelection == "paper" ? winner = "Computer" : winner = "You";
     } else if (playerSelection == "paper") {
-        computerSelection == "seissors" ? winner = "Computer" : winner = "You"
+        computerSelection == "seissors" ? winner = "Computer" : winner = "You";
     } else if (playerSelection == "seissors") {
-        computerSelection == "rock" ? winner = "Computer" : winner = "You"
+        computerSelection == "rock" ? winner = "Computer" : winner = "You";
     }
     
-    let string
+    let string;
 
     if (winner == "Computer") {
-        string = computerSelection[0].toUpperCase() + computerSelection.substring(1) + " beats " + playerSelection[0].toUpperCase() + playerSelection.substring(1)
+        string = computerSelection[0].toUpperCase() + computerSelection.substring(1) + " beats " + playerSelection[0].toUpperCase() + playerSelection.substring(1);
+        scoreCOunter(winner);
     } else if (winner == "You") {
-        string = playerSelection[0].toUpperCase() + playerSelection.substring(1) + " beats " + computerSelection[0].toUpperCase() + computerSelection.substring(1)
+        string = playerSelection[0].toUpperCase() + playerSelection.substring(1) + " beats " + computerSelection[0].toUpperCase() + computerSelection.substring(1);
+        scoreCOunter(winner);
     }
 
-    return winner + " wins! " + string
-
+    return winner + " wins! " + string;
 }
 
-console.log(playRound(playerSelection, computerSelection = getComputerChoise()))
+let result = document.querySelector('.result');
 
-let numberOfRounds = 5
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        result.textContent = playRound(button.id, getComputerChoise());
+    });
+});
 
-function game(numberOfRounds) {
-    for (let i = 0; i < numberOfRounds; i++) {
-        console.log(playRound(playerSelection, computerSelection = getComputerChoise()))
+const finalResult = document.querySelector('.final-result');
+
+const com = document.querySelector('#com');
+const you = document.querySelector('#you');
+
+let comScore = 0;
+let youScore = 0;
+
+function scoreCOunter(winner) {
+    if (winner == "Computer") {
+        com.textContent = ++comScore;
+    } else if (winner == "You") {
+        you.textContent = ++youScore;
+    }
+    
+    if (comScore >= 5) {
+        finalResult.textContent = "Computer Win !";
+        comScore = 0;
+        youScore = 0;
+        com.textContent = comScore;
+        you.textContent = youScore;
+    } else if (youScore >= 5) {
+        finalResult.textContent = "You Win !";
+        comScore = 0;
+        youScore = 0;
+        com.textContent = comScore;
+        you.textContent = youScore;
+    }
+
+    if (comScore + youScore == 1) {
+        finalResult.textContent = "";
     }
 }
-
-console.log(game(numberOfRounds))
